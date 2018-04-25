d782fc3b59669dc10654d7ee1ec7d38ac8cf1ca7
Racdi <rpclerici@gmail.com>
2018-04-25 13:59:19 -0300

    Fix Elipse
    Mudada a maneira de desenhar elipses para ficar menos bugoso

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>
 #include <iostream>
-#include <glut.h>		//Eu removi o GL/ para executar no Visual Studio, recoloquem se precisar
+#include <GL/glut.h>		
 #include <cmath>
 
 GLint WINDOW_WIDTH  = 500,
@@ -127,12 +127,13 @@ void TestTriangle::draw(){
 }
 
 void drawEllipse(float posX, float posY, float radiusX, float radiusY, float r, float g, float b) {
-	glBegin(GL_LINE_LOOP);	//Inicia o processo de desenhar uma elipse
-	glColor3f(r, g, b);
-	for (int d = 0; d < 360; d++) {
+	glBegin(GL_TRIANGLE_FAN);	//Inicia o processo de desenhar uma elipse
+	glColor3f(r, g, b);			//Cria um conjunto de triângulos com um ponto no centro da elipse
+	glVertex2f(posX , posY);
+
+	for (int d = 0; d <= 360; d++) {	//E os outros dois pontos nas bordas, completando um círculo de 360 graus
 		float rad = d / rad2Deg;
 		glVertex2f(posX + cos(rad)*radiusX, posY + sin(rad)*radiusY);
-		glVertex2f(posX , posY);
 	}
 	glEnd();
 }
@@ -179,7 +180,7 @@ void render()
 {
   glClear(GL_COLOR_BUFFER_BIT);
   
-  std::cout<<"Desenho\n";
+  //std::cout<<"Desenho\n";
 
   tri.draw();
   
